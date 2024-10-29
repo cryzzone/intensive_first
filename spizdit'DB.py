bc639cb9edb494cd5e18582b31bf5aef56958837
Reichsfuhrer <lexa88005353535@yandex.ru>
2024-10-29 21:37:40 +0300

    big fix
    я почистил файловую систему от мусора, и дописал скрипт парсера

--- spizdit'DB.py
+++ spizdit'DB.py
@@ -1,8 +1,20 @@
 import cianparser
-import pandas
-
-moscow_parser = cianparser.CianParser(location="Москва")
-data = moscow_parser.get_flats(deal_type="sale", rooms=(1, 2, 3, 4, 5), with_saving_csv=True, additional_settings={"start_page":1, "end_page":54}, with_extra_data=True)
+import pandas as pd
+import time
 
 
-print(data[0,'%'])
+moscowLoc = cianparser.CianParser(location='Москва')
+
+
+data = []
+for i in range(1, 10):
+    a = moscowLoc.get_flats(deal_type='sale', rooms=(5), additional_settings={'start_page': i, 'end_page':i} )
+    data.extend(a)
+    time.sleep(8)
+
+
+export_file = pd.DataFrame(data)
+columns = ['author', 'author_type', 'location', 'deal_type', 'accommodation_type', 'floors_count', 'rooms_count', 'total_meters', 'price', 'district', 'street', 'house_number', 'underground', 'residential_complex']
+selected_columns = export_file[columns]
+
+selected_columns.to_csv('cian.csv', mode='a', header=False, index=False)
