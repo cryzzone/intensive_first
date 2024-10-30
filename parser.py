9df03343493dea8b8027be5854ae30285f56dd2d
Reichsfuhrer <lexa88005353535@yandex.ru>
2024-10-30 12:51:43 +0300

    more db

--- parser.py
+++ parser.py
@@ -7,7 +7,7 @@ moscowLoc = cianparser.CianParser(location='Москва')
 
 
 data = []
-for i in range(1, 10):
+for i in range(300, 400):
     a = moscowLoc.get_flats(deal_type='sale', rooms=(5), additional_settings={'start_page': i, 'end_page':i} )
     data.extend(a)
     time.sleep(8)
@@ -17,4 +17,4 @@ export_file = pd.DataFrame(data)
 columns = ['author', 'author_type', 'location', 'deal_type', 'accommodation_type', 'floors_count', 'rooms_count', 'total_meters', 'price', 'district', 'street', 'house_number', 'underground', 'residential_complex']
 selected_columns = export_file[columns]
 
-selected_columns.to_csv('cian.csv', mode='a', header=False, index=False)
+selected_columns.to_csv('cian-db.csv', mode='a', header=False, index=False)
