f8e2e88a2224efb5b0c30e999bd2fde3ca3849be
Костюк Алексей <96257719+cryzzone@users.noreply.github.com>
2024-11-01 10:12:25 +0300

    fast fix

--- Parsing.py
+++ Parsing.py
@@ -5,7 +5,7 @@ import time
 moscow = cianparser.CianParser(location='Москва')
 
 data = []
-for i in range(1, 2):
+for i in range(1, 3):
     a = moscow.get_flats(deal_type='sale', rooms=(3), additional_settings={'start_page': i, 'end_page':i} )
     data.extend(a)
     time.sleep(3)
@@ -13,4 +13,4 @@ for i in range(1, 2):
 exel_export = pd.DataFrame(data)
 columns = ['author', 'author_type', 'location', 'deal_type', 'accommodation_type', 'floors_count', 'rooms_count', 'total_meters', 'price', 'district', 'street', 'house_number', 'underground', 'residential_complex']
 selected_columns = exel_export[columns]
-selected_columns.to_csv('D:\git\intensive_first\result.csv', mode='a', header=False, index=False)
+selected_columns.to_csv('C:\Users\Silv3\OneDrive\Рабочий стол\intensive_first\result.csv', mode='a', header=False, index=False)
